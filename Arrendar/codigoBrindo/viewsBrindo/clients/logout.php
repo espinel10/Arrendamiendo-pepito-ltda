018a3677a273373103e7d9c78089f81fc8fc8a22
Alejandro Espinel <42283239+espinel10@users.noreply.github.com>
2021-03-23 13:26:46 -0500

    Delete Arrendar directory

--- Arrendar/codigoBrindo/viewsBrindo/clients/logout.php
+++
@@ -1,7 +0,0 @@
-<?php 
-
-include_once '../../scripts/clients/client_login_functions.php';
-
-logoutClient();
-
-?>
--- Arrendar/codigoBrindo/viewsBrindo/employees/logout.php
+++
@@ -1,7 +0,0 @@
-e<?php 
-
-include_once '../../scripts/employees/employee_login_functions.php';
-
-logoutEmployee();
-
-?>
